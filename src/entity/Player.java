0920c433e8a32a6b995b93873fd99ca0845edefc
AsapShadzy <daniel.benzaken1@gmail.com>
2026-05-04 23:02:52 +0300

    Optimized, Larger map, More tiles and tile types, 2D array txt loader... Player in center of screen...

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,18 +15,24 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
 
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = (gp.screenWidth / 2) - (gp.tileSize / 2);
+        screenY = (gp.screenHeight / 2) - (gp.tileSize / 2);
 
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = 23 * gp.tileSize;
+        worldY = 21 * gp.tileSize;
         speed = 4;
         direction = "down";
     }
@@ -63,16 +69,16 @@ public class Player extends Entity {
 
             if (keyH.upPressed) {
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             } else if (keyH.downPressed) {
                 direction = "down";
-                y += speed;
+                worldY += speed;
             } else if (keyH.leftPressed) {
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             } else if (keyH.rightPressed) {
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
 
             spriteCounter++;
@@ -138,7 +144,7 @@ public class Player extends Entity {
 
         if (image != null) {
 
-            g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 
         }
     }
